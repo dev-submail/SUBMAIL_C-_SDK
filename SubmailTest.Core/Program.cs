49a086c2632c14b302cc75e5b6130c9701a0e015
guoqiang <guoqiang@custouch.com>
2016-08-19 10:37:53 +0800

    移植 到 dotnet core

---
+++ SubmailTest.Core/Program.cs
@@ -0,0 +1,34 @@
+﻿using Submail;
+using Submail.Utility;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SubMailTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var jsonreturn = "{\"status\":\"success\",\"return \":[{\"send_id\":\"ner5g3\",\"to\":\"guoqiang @custouch.com\"}]}";
+
+            Console.WriteLine(HttpWebHelper.CheckReturnJsonStatus(jsonreturn));
+            //MailSendDemo mailSendDemo = new MailSendDemo();
+            //mailSendDemo.SendMail();
+
+            // MessageSendXDemo messageSendXDemo = new MessageSendXDemo();
+            // messageSendXDemo.SendMessage();
+
+            // MessageMultiXSendDemo messageMultiXSendDemo = new MessageMultiXSendDemo();
+            // messageMultiXSendDemo.SendMultiMessage();
+
+            //VoiceSendDemo voiceSendDemo = new VoiceSendDemo();
+            //voiceSendDemo.VoiceVerify();
+
+            Console.ReadLine();
+        }
+    }
+}
